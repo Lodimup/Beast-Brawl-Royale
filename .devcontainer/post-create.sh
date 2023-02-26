84fbbc4b039b1ad1d2bbf4672a07e38a117f2891
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2023-02-26 16:44:54 +0000

    INIT

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'export $(cat ${PWD}/.env.local | xargs)' >> ~/.bashrc
+echo 'export $(cat /workspaces/Beast-Brawl-Royale/.env.local | xargs)' >> ~/.bashrc
 git config --global --add safe.directory /workspace
 git config core.fileMode false
 poetry config virtualenvs.in-project true
